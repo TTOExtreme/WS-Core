6772e00c046542e866a78c98c12ec926716ef231
Lucas <cmarottol@hotmalil.com>
2019-11-12 08:57:32 -0300

    Fix _Install
    -- Error on creating user it finishes before atrrib the permissions to user

--- bin/rotine/sql/create/data.js
+++ bin/rotine/sql/create/data.js
@@ -13,30 +13,34 @@ const exe = (callback) => {
     require('../insert/users/add')("admin", "Administrador", bcypher.sha512("admin"), bcypher.sha512("admin" + bcypher.sha512("admin") + bcypher.ger_crypt()), 1, 1, () => {
 
         //add Admininstradores group
-        require('../insert/groups/add')("Administradores", "Administradores", bcypher.ger_crypt(), 1, 1, () => { });
+        require('../insert/groups/add')("Administradores", "Administradores", bcypher.ger_crypt(), 1, 1, () => {
 
-        //add user admin to Administradores groups
-        require('../insert/users/groups')(1, 1, 1, 1, () => { });
+            //add user admin to Administradores groups
+            require('../insert/users/groups')(1, 1, 1, 1, () => {
 
-        //request data on root folder
-        dbdata = JSON.parse(fs.readFileSync(__dirname + "/../../../configs/dbdata.json", 'utf8'));
+                //request data on root folder
+                dbdata = JSON.parse(fs.readFileSync(__dirname + "/../../../configs/dbdata.json", 'utf8'));
 
-        //load all the modules  
-        fs.readdirSync(path.join(__dirname + "/../../../modules/")).forEach((e) => {
-            let dpath = path.join(__dirname + "/../../../modules/" + e + "/server/configs/dbdata.json")
-            if (fs.existsSync(dpath)) {
-                dbdata.tables = dbdata.tables.concat(JSON.parse(fs.readFileSync(dpath, 'utf8')).tables);
-            }
-        })
+                //load all the modules  
+                fs.readdirSync(path.join(__dirname + "/../../../modules/")).forEach((e) => {
+                    let dpath = path.join(__dirname + "/../../../modules/" + e + "/server/configs/dbdata.json")
+                    if (fs.existsSync(dpath)) {
+                        dbdata.tables = dbdata.tables.concat(JSON.parse(fs.readFileSync(dpath, 'utf8')).tables);
+                    }
+                })
 
-        //add data to tables
-        insertData(dbdata, () => {
-            userCrt(50, () => {
-                grpCrt(80, () => {
+                //add data to tables
+                insertData(dbdata, () => {
+                    /*userCrt(50, () => {
+                        grpCrt(80, () => {
+                            callback();
+                        })
+                    })
+                    //*/
                     callback();
                 })
-            })
-        })
+            });
+        });
 
     });
 
@@ -48,20 +52,36 @@ const exe = (callback) => {
 function insertData(data, callback) {
     if (data.tables != undefined) {
         if (data.tables.length > 0) {
-            data.tables.forEach((table) => {
-                if (table.data != undefined) {
-                    if (table.data.length > 0) {
-                        table.data.forEach((dat, datIndex) => {
-                            require('../insert/dataToTables.js')(table.name, table.struct, dat, () => { })
-                        })
-                    }
-                } else {
-                    console.log(table);
-                }
+            subTableInsert(data.tables, () => {
+                callback();
             })
         }
     }
-    callback();
+}
+
+function subTableInsert(tableList, callback) {
+    if (tableList.length > 0) {
+        let table = tableList.pop();
+        if (table.data != undefined) {
+            if (table.data.length > 0) {
+                subdataInsert(table.name, table.struct, table.data, () => {
+                    subTableInsert(tableList, callback);
+                })
+            }
+        }
+    } else {
+        callback();
+    }
+}
+
+function subdataInsert(tableName, tableStruct, dataList, callback) {
+    require('../insert/dataToTables.js')(tableName, tableStruct, dataList.pop(), () => {
+        if (dataList.length > 0) {
+            subdataInsert(tableName, tableStruct, dataList, callback);
+        } else {
+            callback();
+        }
+    })
 }
 
 function userCrt(id, callback) {
