8f80ea2e1bbe92987094251f1ddc36d612c30571
TTOExtreme <ttoextreme@gmai.com>
2020-06-04 08:22:21 -0300

    Iniciando estrutura da parte Web
    -- alterado para ter dois sites (root e Adminstrativo)

--- server/core/socket/webserver.js
+++ server/core/socket/webserver.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const Express = require('express');
 const SocketIO = require('socket.io');
-const colors = require('colors');  
+const colors = require('colors');
 
 const bodyParser = require('body-parser');
 
@@ -33,14 +33,14 @@ class WServer {
      * init
      */
     init() {
-        this._log.task("webserver","Inicializing Webserver",0);
+        this._log.task("webserver", "Inicializing Webserver", 0);
         this._app = Express();
 
         this._server = http.createServer(this._app);
         this._io = SocketIO(this._server);
 
         this._server.listen(this._config.webPort, () => {
-            this._log.task("webserver","Webserver on Port: " + colors.green(this._config.webPort),1);
+            this._log.task("webserver", "Webserver on Port: " + colors.green(this._config.webPort), 1);
         });
 
         this._webhost();
@@ -93,12 +93,13 @@ class WServer {
         this._app.get("/", (req, res) => {
             let cookies = this._parseCookies(req);
             if ((cookies["wscore"])) {//check if cookie is present and redirect if is not
-                res.sendFile('./home.html', { root: this._config.webpageFolder });
+                res.sendFile('./home.html', { root: path(this._config.webpageFolder + "/root") });
             } else {
                 res.redirect(302, "./login")
             }
         })
-        this._app.use(Express.static(this._config.webpageFolder))
+        this._app.use(Express.static(path(this._config.webpageFolder + "/root")))
+        this._app.use("/Administrativo", Express.static(path(this._config.webpageFolder + "/Administrativo")))
         this._hostModules();
     }
 
@@ -108,9 +109,9 @@ class WServer {
     _hostModules() {
         let modulesList = fs.readdirSync(path(__dirname + "../../../modules/"));
         modulesList.forEach(mod => {
-            this._log.task("loading-"+mod,"Loading Module: " + mod,0);
+            this._log.task("loading-" + mod, "Loading Module: " + mod, 0);
             if (fs.existsSync(path(__dirname + "../../../modules/" + mod + "/web"))) {
-                this._log.task("loading-"+mod,"Loaded Module: " + mod,1);
+                this._log.task("loading-" + mod, "Loaded Module: " + mod, 1);
                 this._app.use("/module/" + mod + "/", Express.static(path(__dirname + "../../../modules/" + mod + "/web")));
             }
         })
