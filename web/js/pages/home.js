fe137fc46bb3a3e398406f45d6b2b0965850f7c9
TTOExtreme <ttoextreme@gmai.com>
2021-06-16 15:36:07 -0300

    - Fix Carregamento de JS depois do ClientEvents

--- web/js/pages/home.js
+++ web/js/pages/home.js
@@ -1,9 +1,57 @@
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    function EndLoad(i = 0) {
+        if (ClientEvents != undefined) {
+            ClientEvents.setCoreEvent("LoadExternal");
+            ClientEvents.on("LoadExternal", (list, callback, refreshOnError = true, index = 5) => {
+                loadExternalFiles(list).then(() => {
+                    if (callback != undefined) {
+                        callback();
+                    } else {
+                        //console.log("Callback Null");
+                        //console.log(list)
+                        //console.log(index);
+                    }
+                }).catch((err) => {
+                    console.log("An error ocurred when loading external css\nAre you disconnected from internet?")
+                    console.log(err);
+                    if (index <= 0) {
+                        if (refreshOnError) {
+                            window.location.reload();
+                        }
+                    } else {
+                        setTimeout(() => {
+                            ClientEvents.emit("LoadExternal", list, callback, index - 1);
+                        })
+                    }
+                })
+
+            })
+            ClientEvents.emit("Page_Loaded");
+            document.body.style.opacity = 1;
+            ClientEvents.emit("LoadExternal", [
+                "./js/libs/suneditor.min.js"
+            ], () => {
+                ClientEvents.emit("system_mess", {
+                    status: "INFO",
+                    mess: "Todos os Modulos Carregados",
+                    time: 1000
+                })
+            });
+        } else {
+            if (i < 50) {
+                setTimeout(() => {
+                    EndLoad(i + 1);
+                }, 100);
+            } else {
+                window.location.reload();//força o reload da pagina caso aconteça algum erro
+            }
+        }
+    }
+
     document.body.style.opacity = 0;
     loadExternalFiles([
-        "./css/screen/suneditor.min.css",
         "./js/utils/eventHandler.js",
         "./js/libs/socket.io.js",
         "./js/libs/crypto-js.js",
@@ -21,42 +69,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         "./js/core/user/changepass.js",
         "./js/libs/tabulator.min.js",
     ]).then(() => {
-        ClientEvents.setCoreEvent("LoadExternal");
-        ClientEvents.on("LoadExternal", (list, callback, refreshOnError = true, index = 5) => {
-            loadExternalFiles(list).then(() => {
-                if (callback != undefined) {
-                    callback();
-                } else {
-                    //console.log("Callback Null");
-                    //console.log(list)
-                    //console.log(index);
-                }
-            }).catch((err) => {
-                console.log("An error ocurred when loading external css\nAre you disconnected from internet?")
-                console.log(err);
-                if (index <= 0) {
-                    if (refreshOnError) {
-                        window.location.reload();
-                    }
-                } else {
-                    setTimeout(() => {
-                        ClientEvents.emit("LoadExternal", list, callback, index - 1);
-                    })
-                }
-            })
-
-        })
-        ClientEvents.emit("Page_Loaded");
-        document.body.style.opacity = 1;
-        ClientEvents.emit("LoadExternal", [
-            "./js/libs/suneditor.min.js"
-        ], () => {
-            ClientEvents.emit("system_mess", {
-                status: "INFO",
-                mess: "Todos os Modulos Carregados",
-                time: 1000
-            })
-        });
+        EndLoad(0);
     }).catch((err) => {
         console.log("An error ocurred when loading external files\nAre you disconnected from internet?")
         console.log(err);
@@ -75,6 +88,7 @@ loadExternalFiles([
     "./css/screen/contextCreator.css",
     "./css/fontAwesome.min.css",
     "./css/themes/theme-dark.css",
+    "./css/screen/suneditor.min.css",
 ]).then(() => {
 }).catch((err) => {
     console.log("An error ocurred when loading external css\nAre you disconnected from internet?")
