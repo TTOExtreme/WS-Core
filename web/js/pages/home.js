913042e5998c2853d78fb63cc6221d202ff2b2b0
TTOExtreme <ttoextreme@gmai.com>
2021-06-16 15:39:37 -0300

    - Carregamento de JS Externo

--- web/js/pages/home.js
+++ web/js/pages/home.js
@@ -3,42 +3,63 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     function EndLoad(i = 0) {
         if (ClientEvents != undefined) {
-            ClientEvents.setCoreEvent("LoadExternal");
-            ClientEvents.on("LoadExternal", (list, callback, refreshOnError = true, index = 5) => {
-                loadExternalFiles(list).then(() => {
-                    if (callback != undefined) {
-                        callback();
-                    } else {
-                        //console.log("Callback Null");
-                        //console.log(list)
-                        //console.log(index);
-                    }
-                }).catch((err) => {
-                    console.log("An error ocurred when loading external css\nAre you disconnected from internet?")
-                    console.log(err);
-                    if (index <= 0) {
-                        if (refreshOnError) {
-                            window.location.reload();
-                        }
-                    } else {
-                        setTimeout(() => {
-                            ClientEvents.emit("LoadExternal", list, callback, index - 1);
-                        })
-                    }
-                })
+            loadExternalFiles([
 
-            })
-            ClientEvents.emit("Page_Loaded");
-            document.body.style.opacity = 1;
-            ClientEvents.emit("LoadExternal", [
-                "./js/libs/suneditor.min.js"
-            ], () => {
-                ClientEvents.emit("system_mess", {
-                    status: "INFO",
-                    mess: "Todos os Modulos Carregados",
-                    time: 1000
+                "./js/screen/loading.js",
+                "./js/utils/formaters.js",
+                "./js/screen/systemMessage.js",
+                "./js/utils/socket.js",
+                "./js/libs/bcypher.js",
+                "./js/libs/jkanban.min.js",
+                "./js/screen/favoritos.js",
+                "./js/screen/menuDragger.js",
+                "./js/screen/credits.js",
+                "./js/screen/sideMenu.js",
+                "./js/screen/contextCreator.js",
+                "./js/core/user/changepass.js",
+                "./js/libs/tabulator.min.js",
+            ]).then(() => {
+                ClientEvents.setCoreEvent("LoadExternal");
+                ClientEvents.on("LoadExternal", (list, callback, refreshOnError = true, index = 5) => {
+                    loadExternalFiles(list).then(() => {
+                        if (callback != undefined) {
+                            callback();
+                        } else {
+                            //console.log("Callback Null");
+                            //console.log(list)
+                            //console.log(index);
+                        }
+                    }).catch((err) => {
+                        console.log("An error ocurred when loading external css\nAre you disconnected from internet?")
+                        console.log(err);
+                        if (index <= 0) {
+                            if (refreshOnError) {
+                                window.location.reload();
+                            }
+                        } else {
+                            setTimeout(() => {
+                                ClientEvents.emit("LoadExternal", list, callback, index - 1);
+                            })
+                        }
+                    })
+
                 })
-            });
+                ClientEvents.emit("Page_Loaded");
+                document.body.style.opacity = 1;
+                ClientEvents.emit("LoadExternal", [
+                    "./js/libs/suneditor.min.js"
+                ], () => {
+                    ClientEvents.emit("system_mess", {
+                        status: "INFO",
+                        mess: "Todos os Modulos Carregados",
+                        time: 1000
+                    })
+                });
+            }).catch(err => {
+                console.log("An error ocurred when loading external files\nAre you disconnected from internet?")
+                console.log(err);
+                setTimeout(() => { window.location.reload(); }, 1000);
+            })
         } else {
             if (i < 50) {
                 setTimeout(() => {
@@ -55,19 +76,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         "./js/utils/eventHandler.js",
         "./js/libs/socket.io.js",
         "./js/libs/crypto-js.js",
-        "./js/screen/loading.js",
-        "./js/utils/formaters.js",
-        "./js/screen/systemMessage.js",
-        "./js/utils/socket.js",
-        "./js/libs/bcypher.js",
-        "./js/libs/jkanban.min.js",
-        "./js/screen/favoritos.js",
-        "./js/screen/menuDragger.js",
-        "./js/screen/credits.js",
-        "./js/screen/sideMenu.js",
-        "./js/screen/contextCreator.js",
-        "./js/core/user/changepass.js",
-        "./js/libs/tabulator.min.js",
     ]).then(() => {
         EndLoad(0);
     }).catch((err) => {
