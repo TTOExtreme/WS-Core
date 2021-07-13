de24c9197eb695a6643cfd47d8fd693b63199f15
TTOExtreme <ttoextreme@gmai.com>
2021-07-13 13:21:54 -0300

    - Adicionar multiplos anexos a OS - Adicionado campo de data de adição do anexo

--- server/modules/WSOP/web/js/utils/anexo.js
+++ server/modules/WSOP/web/js/utils/anexo.js
@@ -13,7 +13,7 @@ ClientEvents.on("WSOP/os/anexo/edt", (data) => {
     div.innerHTML = "" +
         "<table>" +
         "<tr><td id='move_menu_wsop_add' class='move_menu' onmousedown=ClientEvents.emit(\"move_menu_down\",'wsop_anexo_div')>&#9776;</td><td class='wsop_edt_label'><p class='wsop_add_closeButton' onclick='ClientEvents.emit(\"WSOP/os/anexo/close\")'>X</p></td></tr>" +
-        "<tr><td><center>" + (data.name || "") + "</center></td></tr>" +
+        "<tr><td><center><pre>Nome: " + data.name + "\nData: " + formatTime(data.createdIn) + "</pre></center></td></tr>" +
         "<tr><td><img class='wsop_anexo_img' alt='' src='./module/WSOP/img/" + (data.replace_img || data.filename) + "'></td></tr>" +
         "<tr><td><a id='download_img' href=\"./module/WSOP/img/" + data.filename + "\" download=\"" + (data.name || "anexo") + data.filename.substring(data.filename.lastIndexOf(".")) + "\"></a></td></tr>" +
         "<tr><td><input id='wpma_sites_submit' value='Excluir' type='button'  onclick='ClientEvents.emit(\"SendSocket\",\"wsop/os/anexo/del\", {id:" + data.id + ",id_os:" + data.id_os + "})'><input id='wpma_sites_submit' value='Download' type='button' onclick='document.getElementById(\"download_img\").click()'></td></tr>" +
@@ -37,7 +37,7 @@ ClientEvents.on("WSOP/os/anexo/view", (data) => {
     div.innerHTML = "" +
         "<table>" +
         "<tr><td id='move_menu_wsop_add' class='move_menu' onmousedown=ClientEvents.emit(\"move_menu_down\",'wsop_anexo_div')>&#9776;</td><td class='wsop_edt_label'><p class='wsop_add_closeButton' onclick='ClientEvents.emit(\"WSOP/os/anexo/close\")'>X</p></td></tr>" +
-        "<tr><td><center>" + (data.name || "") + "</center></td></tr>" +
+        "<tr><td><center><pre>Nome: " + data.name + "\nData: " + formatTime(data.createdIn) + "</pre></center></td></tr>" +
         "<tr><td><img class='wsop_anexo_img' alt='' src='./module/WSOP/img/" + data.filename + "'></td></tr>" +
         "<tr><td><a id='download_img' href=\"./module/WSOP/img/" + data.filename + "\" download=\"" + (data.name || "imagem") + data.filename.substring(data.filename.lastIndexOf(".")) + "\"></a></td></tr>" +
         "<tr><td><input id='wpma_sites_submit' value='Download' type='button' onclick='document.getElementById(\"download_img\").click()'></td></tr>" +
